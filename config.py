efce1c2f5c64e0566bdecf5aba4b4aba1d52fa21
paria44pb-maker <paria44.pb@gmail.com>
2026-05-13 22:26:19 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+NOBITEX_API_KEY = os.getenv("NOBITEX_API_KEY")
