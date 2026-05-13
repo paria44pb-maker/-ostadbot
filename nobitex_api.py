d1495ba552642ce55a4342d308b964e815bf6a9c
paria44pb-maker <paria44.pb@gmail.com>
2026-05-13 17:49:22 +0330

    Create nobitex_api.py

---
+++ nobitex_api.py
@@ -0,0 +1,13 @@
+import requests
+import os
+
+# تنظیمات پایه و توکن‌ها
+
+def get_balance():
+    # دریافت موجودی
+
+def place_order(side, symbol, amount):
+    # ثبت سفارشات
+
+def get_market_price(symbol):
+    # دریافت قیمت لحظه‌ای
