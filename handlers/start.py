37035538a48301769909a76bf3717d3b4d715bed
paria44pb-maker <paria44.pb@gmail.com>
2026-05-13 22:28:25 +0330

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,11 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def start(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE
+):
+
+    await update.message.reply_text(
+        "ربات حرفه‌ای کریپتو فعال شد 🚀"
+    )
