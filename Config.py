215ed1a483641cbf82a1e35c291322ee78661f08
paria44pb-maker <paria44.pb@gmail.com>
2026-05-16 02:00:10 +0330

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,7 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+COINGECKO = "https://api.coingecko.com/api/v3"
+BINANCE = "https://api.binance.com/api/v3"
