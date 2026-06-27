b741ff2ffc287c3736172a02221af12023fa70a4
paria44pb-maker <paria44.pb@gmail.com>
2026-06-27 23:38:42 +0330

    Update startup.py

--- core/startup.py
+++ core/startup.py
@@ -1,5 +1,10 @@
-from core.database import init_db
+from security.validator import validate_env
 
 async def init_app():
-    await init_db()
-    print("System Initialized 🚀")
+    print("🚀 Starting CryptoPulse SAFE MODE...")
+
+    # 1. validate envs first
+    validate_env()
+
+    # 2. later you can add db init
+    print("✅ Core systems loaded safely")
