8fe31b18fcbe40e56b31d808dfa1eca85760e563
paria44pb-maker <paria44.pb@gmail.com>
2026-06-27 23:55:22 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
-import os
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {
-        "status": "ok",
-        "bot_token": "YES" if os.getenv("BOT_TOKEN") else "NO"
-    }
+    print("APP IS RUNNING")
+    return {"status": "ok"}
