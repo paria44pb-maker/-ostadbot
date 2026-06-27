89f7ca3c3082f46187e70d4c898e700c16279a80
paria44pb-maker <paria44.pb@gmail.com>
2026-06-27 20:33:44 +0330

    Delete services directory

--- services/coinex_service.py
+++
@@ -1,7 +0,0 @@
-import aiohttp
-
-async def get_ticker(symbol="BTCUSDT"):
-    url = f"https://api.coinex.com/v2/spot/ticker?market={symbol}"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, timeout=15) as resp:
-            return await resp.json()
