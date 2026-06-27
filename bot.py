99111e23c665ed8f5721562f55ae6a89c7699f48
paria44pb-maker <paria44.pb@gmail.com>
2026-06-27 20:18:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,8 @@
-import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from config import BOT_TOKEN
-from db import init_db
-from handlers.user import router as user_router
-from handlers.callbacks import router as callback_router
-from handlers.admin import router as admin_router
+from config import Config
 
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=Config.BOT_TOKEN)
 dp = Dispatcher()
-dp.include_router(user_router)
-dp.include_router(callback_router)
-dp.include_router(admin_router)
 
-async def main():
-    await init_db()
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+async def setup_bot():
+    print("Bot started...")
