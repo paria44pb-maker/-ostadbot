b62922082f63e1411d54a66db15aad5b61c611ee
paria44pb-maker <paria44.pb@gmail.com>
2026-06-27 21:09:21 +0330

    Create core.py

---
+++ smc-engine/core.py
@@ -0,0 +1,8 @@
+def detect_smc(data):
+    return {
+        "BOS": True,
+        "CHOCH": True,
+        "liquidity_sweep": True,
+        "order_blocks": ["zone1", "zone2"],
+        "score": 85
+    }
