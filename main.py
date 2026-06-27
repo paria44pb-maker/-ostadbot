0cebdc3a6ab86c3a56537d919c93b540943e28bd
paria44pb-maker <paria44.pb@gmail.com>
2026-06-27 23:52:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import asyncio
 from fastapi import FastAPI
-
-from core.startup import init_app
-from telegram.bot_runner import start_bot
+import os
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    await init_app()
-
-    # bot جدا اجرا می‌شود (بدون قفل کردن server)
-    asyncio.create_task(start_bot())
-
 @app.get("/")
 def home():
-    return {"status": "CryptoPulse SAFE RUNNING 🚀"}
-
-@app.get("/health")
-def health():
-    return {"ok": True}
+    return {
+        "status": "ok",
+        "bot_token": "YES" if os.getenv("BOT_TOKEN") else "NO"
+    }
