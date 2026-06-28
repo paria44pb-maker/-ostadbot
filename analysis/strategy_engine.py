eda1fdeec74617798dd4ea35ccaaeb1fae2edafa
paria44pb-maker <paria44.pb@gmail.com>
2026-06-28 12:45:08 +0330

    Delete analysis directory

--- analysis/strategy_engine.py
+++
@@ -1,8 +0,0 @@
-def simple_signal(price_data):
-    try:
-        price = float(price_data["data"]["ticker"]["last"])
-        if price > 50000:
-            return "SELL"
-        return "BUY"
-    except:
-        return "HOLD"
