09d595c563ba7b4f71918dac7c6fef6b6ca12530
paria44pb-maker <paria44.pb@gmail.com>
2026-06-28 12:48:58 +0330

    Delete smc-engine directory

--- smc-engine/core.py
+++
@@ -1,8 +0,0 @@
-def detect_smc(data):
-    return {
-        "BOS": True,
-        "CHOCH": True,
-        "liquidity_sweep": True,
-        "order_blocks": ["zone1", "zone2"],
-        "score": 85
-    }
