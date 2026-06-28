153e89b37d58611e7c66a00c3d72f56ec56cabdf
paria44pb-maker <paria44.pb@gmail.com>
2026-06-28 12:46:56 +0330

    Delete market directory

--- market/coinex.py
+++
@@ -1,9 +0,0 @@
-import aiohttp
-
-BASE = "https://api.coinex.com/v2"
-
-async def ticker(symbol="BTCUSDT"):
-    url = f"{BASE}/spot/ticker?market={symbol}"
-    async with aiohttp.ClientSession() as s:
-        async with s.get(url) as r:
-            return await r.json()
