68e226b51627f817d122a2271e3dd63833a4cf93
paria44pb-maker <paria44.pb@gmail.com>
2026-06-28 12:49:30 +0330

    Delete telegram directory

--- telegram/handlers/start.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-
-router = Router()
-
-@router.message()
-async def start(message: Message):
-    await message.answer("CryptoPulse AI فعال شد 🚀")
