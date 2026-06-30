22f600687fefb9d73eab27bcaf05bb68fbb73d21
paria44pb-maker <paria44.pb@gmail.com>
2026-06-30 22:13:21 +0330

    Update part9.py

--- part9.py
+++ part9.py
@@ -948,7 +948,7 @@ class BotHandlers:
         self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
         
         # Conversation handler
-        conv_handler = ConversationHandler(
+        conv_handler = ConversationHandler
             entry_points=[
                 CommandHandler("signal", signal_command),
                 CallbackQueryHandler(callback_handler, pattern="^analysis$"),
