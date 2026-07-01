017428be4eed18f679132a4c93f092a612042d7e
paria44pb-maker <paria44.pb@gmail.com>
2026-07-01 09:47:26 +0330

    Update server.py

--- server.py
+++ server.py
@@ -23,4 +23,4 @@ async def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port, log_level="error")
+    uvicorn.run(app, host="0.0.0.0", port=port)
